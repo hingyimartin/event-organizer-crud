c7cdb9ae9e3184413b9e89bf4b7eb3661e8400bf
hingyimartin <hingyimartin@gmail.com>
2025-06-02 20:43:26 +0200

    Add register functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,48 @@
+import bcrypt from 'bcrypt';
 import User from '../models/User.js';
 
-export const register = (req, res) => {
-  res.status(200).json({ message: 'ok' });
+export const register = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+
+    // check email
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({
+        message: 'Email already in use',
+      });
+    }
+
+    // check username
+    const usernameExist = await User.findOne({ username });
+    if (usernameExist) {
+      return res.status(400).json({
+        message: 'Username already in use',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role: 'user',
+    });
+    if (!user) {
+      res.status(500).json({ message: 'Error creating user' });
+    }
+    res.status(201).json({
+      message: 'User created successfuly',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `${
+        process.env.NODE_ENV === 'development'
+          ? error
+          : 'An unexpected error happened'
+      }`,
+    });
+  }
 };
 
 export const login = (req, res) => {
